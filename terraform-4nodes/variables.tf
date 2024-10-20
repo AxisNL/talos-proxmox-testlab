f227e3b41a82e14fc4f1427fb4bfc4f9641efabe
Angelo Hongens <angelo@hongens.nl>
2024-10-20 09:30:42 -0400

    update terraform

---
+++ terraform-4nodes/variables.tf
@@ -0,0 +1,15 @@
+variable "pm_api_url" {
+    type = string
+}
+
+variable "pm_api_token_id" {
+    type = string
+}
+
+variable "pm_api_token_secret" {
+    type = string
+}
+
+variable "pm_host" {
+    type = string
+}
